189e1760bac76da1a98395155ea3eab59fad8e60
ki9mu <47977616+ki9mu@users.noreply.github.com>
2022-06-08 23:37:28 +0800

    Update and rename api.py to apilist.py

--- oneforall-config/apilist.py
+++ oneforall-config/apilist.py
@@ -1,26 +1,36 @@
+enable = False
+
 # 模块API配置
 # Censys可以免费注册获取API：https://censys.io/api
+# censys_api_id:censys_api_secret
 censys_api_id = ''
 censys_api_secret = ''
+censys_api_list = [{"aaa": "bbb"}, {"ccc": "ddd"}]
 
 # Binaryedge可以免费注册获取API：https://app.binaryedge.io/account/api
 # 免费的API有效期只有1个月，到期之后可以再次生成，每月可以查询250次。
 binaryedge_api = ''
+binaryedge_api_list = [""]
 
 # Chinaz可以免费注册获取API：http://api.chinaz.com/ApiDetails/Alexa
 chinaz_api = ''
+chinaz_api_list = [""]
 
 # Bing可以免费注册获取API：https://azure.microsoft.com/zh-cn/services/
 # cognitive-services/bing-web-search-api/#web-json
+# bing_api_id:bing_api_key
 bing_api_id = ''
 bing_api_key = ''
+bing_api_list = [{"": ""}]
 
 # SecurityTrails可以免费注册获取API：https://securitytrails.com/corp/api
 securitytrails_api = ''
+securitytrails_api_list = [""]
 
 # https://fofa.so/api
 fofa_api_email = ''  # fofa用户邮箱
 fofa_api_key = ''  # fofa用户key
+fofa_api_list = [{"": ""}]
 
 # Google可以免费注册获取API:
 # 免费的API只能查询前100条结果
@@ -29,48 +39,62 @@ fofa_api_key = ''  # fofa用户key
 google_api_id = ''  # Google API自定义搜索引擎id
 # https://developers.google.com/custom-search/v1/overview#api_key
 google_api_key = ''  # Google API自定义搜索key
+google_api_list = [{"": ""}]
 
 # https://api.passivetotal.org/api/docs/
 riskiq_api_username = ''
 riskiq_api_key = ''
+riskiq_api_list = [{"": ""}]
 
 # Shodan可以免费注册获取API: https://account.shodan.io/register
 # 免费的API限速1秒查询1次
 shodan_api_key = ''
+shodan_api_list = ['']
+
 # ThreatBook API 查询子域名需要收费 https://x.threatbook.cn/nodev4/vb4/myAPI
 threatbook_api_key = ''
+threatbook_api_list = ['']
 
 # VirusTotal可以免费注册获取API: https://developers.virustotal.com/reference
 virustotal_api_key = ''
+virustotal_api_list = ['']
 
 # https://www.zoomeye.org/doc?channel=api
 zoomeye_api_usermail = ''
 zoomeye_api_password = ''
+zoomeye_api_list = [{"": ""}]
 
 # Spyse可以免费注册获取API: https://spyse.com/
 spyse_api_token = ''
+spyse_api_list = ['']
 
 # https://www.circl.lu/services/passive-dns/
 circl_api_username = ''
 circl_api_password = ''
+circl_api_list = [{"": ""}]
 
 # https://www.dnsdb.info/
 dnsdb_api_key = ''
+dnsdb_api_list = ['']
 
 # ipv4info可以免费注册获取API: http://ipv4info.com/tools/api/
 # 免费的API有效期只有2天，到期之后可以再次生成，每天可以查询50次。
 ipv4info_api_key = ''
+ipv4info_api_list = ['']
 
 # https://github.com/360netlab/flint
 # passivedns_api_addr默认空使用http://api.passivedns.cn
 # passivedns_api_token可为空
 passivedns_api_addr = ''
 passivedns_api_token = ''
+passivedns_api_list = [{"": ""}]
 
 # Github Token可以访问https://github.com/settings/tokens生成,user为Github用户名
 # 用于子域接管和子域收集
 github_api_user = ''
 github_api_token = ''
+github_api_list = [{"": ""}]
 
 # obtain Cloudflare API key from https://dash.cloudflare.com/profile/api-tokens
 cloudflare_api_token = ''
+cloudflare_api_list = ['']
